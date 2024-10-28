182aa07ea42ae5203a3d31c178b224de86d08872
Harry42203 <conor.harrington@pm.me>
2024-10-28 16:47:16 +0000

    added pygame sprites

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import pygame
 from constants import *
 
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self,"containers"):
+            super().__init__(self, "containers")
+        else:
+            super().__init__()
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0,0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -13,6 +31,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        #clock.tick(60)
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
