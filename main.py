197ebf72b9db0a8d16f11e9e56b6abc07130c658
Harry42203 <conor.harrington@pm.me>
2024-10-28 18:51:38 +0000

    working on it saving progress

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 import pygame
 from constants import *
-
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        if hasattr(self,"containers"):
-            super().__init__(self, "containers")
-        else:
-            super().__init__()
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0,0)
-        self.radius = radius
-
-    def draw(self, screen):
-        pass
-
-    def update(self, dt):
-        pass
+from circleshape import *
+from player import *
+from asteroid import *
+from asteoidfield import *
 
 def main():
+    # Welcome message in terminal
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Initialise pygame
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Add sprite groups and add objects to groups
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (drawable, updatable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    
+    # Initialise player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+
+    # Main game loop
     while True:
+
+        # Enable pygames close button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # Set background to black    
         screen.fill((0,0,0))
+
+        # iterate through the updateable group and update each object
+        for each in updatable:
+            each.update(dt)
+        # iterate through the drawable group and update each object
+        for item in drawable:
+            item.draw(screen)
+
+        # reset the display
         pygame.display.flip()
-        #clock.tick(60)
-        dt = (clock.tick(60) / 1000)
+        
+        # limit fps to 60 but dont allow slowdown if fps dips
+        MAX_DT = 0.05
+        dt = min(clock.tick(60) / 1000, MAX_DT)
 
 if __name__ == "__main__":
     main()
